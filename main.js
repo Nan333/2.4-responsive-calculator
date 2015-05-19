edbe7c1b63eb93b6d022d1db0d8e089c92d72caa
Nan Chen <nchen333@gmail.com>
2015-05-19 08:49:05 -0400

    number-display

--- main.js
+++ main.js
@@ -1,27 +1,119 @@
-function alertButtonValue(event) {
-  var button = event.target;
-  var text = button.textContent;
-  alert(text);
-}
-
-[].forEach.call(document.querySelectorAll('.word'), function(element){
-  element.addEventListener('click', alertButtonValue);
-}, false);
-
-function alertNumberPlusNumber(event) {
-  var button = event.target;
-  var number = Number(button.textContent);
-  alert(number + number);
-}
-
-[].forEach.call(document.querySelectorAll('.number'), function(element){
-  element.addEventListener('click', alertNumberPlusNumber);
-}, false);
-
-var operator = [+, -, *, /, C]
-
-var operatorPress = function(operator){
-  alert(operator);
+var screenValue = document.querySelector('.screenValue');
+function numberReturn(event) {
+  var number = Number(event.target.textContent);
+  screenValue.textContent = number;
 };
 
-operatorPress(operator);
+[].forEach.call(document.querySelectorAll('.button'), function(element){
+    element.addEventListener('click', numberReturn);
+  }, false);
+
+//his code
+
+
+// var calculation = 0;
+// var displayedCalculation = "0";
+// var pendingOperation;
+// var $display = document.querySelector('.display-number');
+//
+// function numberReturn(event) {
+//   var number = Number(event.target.textContent);
+//   if(pendingOperation !== undefined) {
+//     switch (pendingOperation) {
+//       case "+":
+//         calculation += number;
+//         break;
+//     }
+//     pendingOperation = undefined;
+//     $display.textContent = number;
+//   } else {
+//     displayOrConcatenateNumber(number);
+//   }
+// }
+//
+// function decimalButtonPressed(event) {
+//
+// }
+//
+// function operatorButtonPressed(event) {
+//   pendingOperation = event.target.textContent;
+// }
+//
+// function equalButtonPressed(event) {
+//   $display.textContent = calculation;
+// }
+//
+//   /*
+//     Utility functions
+//   */
+//   function displayOrConcatenateNumber(inputNumber) {
+//     if(calculation === 0) {
+//       calculation = Number(inputNumber);
+//       displayedCalculation = String(inputNumber);
+//     } else {
+//       calculation = Number(displayedCalculation + inputNumber);
+//       displayedCalculation = String(displayedCalculation + inputNumber);
+//     }
+//     $display.textContent = displayedCalculation;
+//   }
+
+  /*
+    Setup event handlers
+  */
+
+  // [].forEach.call(document.querySelectorAll('.keypad-button.number'), function(element){
+  //   element.addEventListener('click', numberButtonPressed);
+  // }, false);
+  //
+  // [].forEach.call(document.querySelectorAll('.keypad-button.operator'), function(element){
+  //   element.addEventListener('click', operatorButtonPressed);
+  // }, false);
+  //
+  // document.querySelector('.decimal').addEventListener('click', decimalButtonPressed);
+  // document.querySelector('.equal').addEventListener('click', equalButtonPressed);
+//
+// })();
+
+//previous code
+
+// function alertButtonValue(event) {
+//   var button = event.target;
+//   var text = button.textContent;
+//   alert('hello');
+// }
+//
+// (function(){
+//     'use strict';
+//     var calculation = 0;
+//     var $display = document.querySelector('.display-number');
+//     function numberButtonPressed(event) {
+//       var number = Number(event.target.textContent);
+//       displayOrConcatenateNumber(number;)
+//     }
+//     function displayOrConcatenateNumber(number){
+//       if(calculation == 0) {
+//         $display.textContent = number;
+//       } else {
+//         calculation = Number(displayedCalculation + number);
+//         $display-screen.textContent = displayedCalculation + number
+//       }
+//     }
+//     [].forEach.call(document.querySelectorAll('.button'), function(element){
+//       element.addEventListener('click', alertButtonValue);
+//     }, false);
+//
+// function operatorButtonPressed(event) {
+//   pendingOperation = event.target.textContent;
+//   displayOrConcatenate(calculation);
+// }
+//
+// function alertNumberPlusNumber(event) {
+//   var button = event.target;
+//   var number = Number(button.textContent);
+//   alert(number + number);
+// }
+//
+// [].forEach.call(document.querySelectorAll('.number'), function(element){
+//   element.addEventListener('click', alertNumberPlusNumber);
+// }, false);
+// })();
